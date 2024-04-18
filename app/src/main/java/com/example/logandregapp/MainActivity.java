521f79eedfb550dee8e310da408dacd277fdf5ee
artemzaharov <artem4ikaz@icloud.com>
2024-04-18 13:12:11 +0300

    Prog

--- app/src/main/java/com/example/logandregapp/MainActivity.java
+++ app/src/main/java/com/example/logandregapp/MainActivity.java
@@ -1,24 +1,130 @@
 package com.example.logandregapp;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.logandregapp.models.users;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnsigin, btnreg;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference user;
+
+    RelativeLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnreg = findViewById(R.id.btnreg);
+        btnsigin = findViewById(R.id.btnsigin);
+        root = findViewById(R.id.root_element);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        user = db.getReference("Users");
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterWindow();
+            }
         });
+    }
+
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Пожалуйста заполните данные");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View reg_w = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(reg_w);
+        final MaterialEditText email = reg_w.findViewById(R.id.email);
+        final MaterialEditText password = reg_w.findViewById(R.id.passf);
+        final MaterialEditText name = reg_w.findViewById(R.id.name);
+        final MaterialEditText phone = reg_w.findViewById(R.id.phonef);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+                if (TextUtils.isEmpty(email.getText().toString()))
+                {
+                    Snackbar.make(root, "Введите почту", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(name.getText().toString()))
+                {
+                    Snackbar.make(root, "Введите имя", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(phone.getText().toString()))
+                {
+                    Snackbar.make(root, "Введите телефон", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                if (password.getText().toString().length() < 8)
+                {
+                    Snackbar.make(root, "Введите почту", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                users Userr = new users();
+                                Userr.setEmail(email.toString());
+                                Userr.setName(name.toString());
+                                Userr.setPass(password.toString());
+                                Userr.setPhone(phone.toString());
+                                user.child(Userr.getEmail()).setValue(Userr)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Snackbar.make(root, "Пользователь добавлен!", Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+
+                            }
+                        });
+            }
+        });
+
+        dialog.show();
     }
 }
